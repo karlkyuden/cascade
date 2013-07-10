052cbc6dc33a0f8be4e8d2a0313edf6f188e3e2d
Oliver <drrsxracer@sbcglobal.net>
2013-07-09 21:44:31 -0700

    Create Randomizer.java

---
+++ Randomizer.java
@@ -0,0 +1,30 @@
+package com.example.cascade;
+
+import android.widget.ToggleButton;
+import java.util.Random;
+
+public class Randomizer{
+
+  public void random (ToggleButton position1, ToggleButton position2, ToggleButton position3){
+		
+		int colorArray[] = {R.drawable.red_circle, R.drawable.green_circle, R.drawable.blue_circle};
+		
+		Random rand = new Random();
+		
+		int randomColor, randomColor2, randomColor3 = colorArray[0];
+		
+		randomColor = rand.nextInt(colorArray.length);
+		position1.setButtonDrawable(colorArray[randomColor]);
+		
+		do{
+			randomColor2 = rand.nextInt(colorArray.length);
+			position2.setButtonDrawable(colorArray[randomColor2]);}
+		while(randomColor2 == randomColor);
+		
+		do{
+			randomColor3 = rand.nextInt(colorArray.length);
+			position3.setButtonDrawable(colorArray[randomColor3]);}
+		while(randomColor3 == randomColor || randomColor3 == randomColor2);
+	}
+	
+}
